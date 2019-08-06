50307c1cebe06e8b130dfc54981f423143d8dd7c
Rishabh Kumar <rishabh.jarvis@gmail.com>
2019-08-06 23:04:33 +0530

    second

--- src/app.js
+++ src/app.js
@@ -9,7 +9,27 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.get('/', (req, res) => res.render('index', {title: 'Index'}))
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8')
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8')
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts}));
+
+app.get('/saving', (req, res) => {
+  res.render('account', {account: accounts.savings})
+})
+app.get('/checking', (req, res) => {
+  res.render('account', {account: accounts.checking});
+})
+app.get('/credit', (req, res) => {
+  res.render('account', {account: accounts.credit});
+})
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {user: users[0]});
+})
 
 app.listen(3000, () => console.log('Port running on http://localhost:3000'))
